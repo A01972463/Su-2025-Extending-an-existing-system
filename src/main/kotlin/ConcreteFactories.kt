ffa84a892a5247d54eb5bca49ff66a5839792bde
Gustavo Tavo Huerta <146301984+A01972463@users.noreply.github.com>
2025-07-23 14:15:44 -0600

    Added multiple Factories and Products

---
+++ src/main/kotlin/ConcreteFactories.kt
@@ -0,0 +1,21 @@
+class ConcreteFactory1: AbstractFactory() {
+    override fun overrideMethod(type: String): AbstractProduct {
+        return when (type) {
+            "1" -> ConcreteProductA1()
+            "2" -> ConcreteProductA2()
+            "3" -> ConcreteProductA3()
+            else -> NullProduct()
+        }
+    }
+}
+
+class ConcreteFactory2: AbstractFactory() {
+    override fun overrideMethod(type: String): AbstractProduct {
+        return when (type) {
+            "1" -> ConcreteProductB1()
+            "2" -> ConcreteProductB2()
+            "3" -> ConcreteProductB3()
+            else -> NullProduct()
+        }
+    }
+}
